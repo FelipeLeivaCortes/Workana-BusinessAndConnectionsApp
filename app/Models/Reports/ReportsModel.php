b4ee29d9c8e4b476e6d9fca22ba49804c73b9618
Felipe Leiva <felipe.leiva@solutoria.cl>
2024-10-23 19:21:41 -0300

    Actualización reporte ordenes

--- app/Models/Reports/ReportsModel.php
+++ app/Models/Reports/ReportsModel.php
@@ -39,31 +39,37 @@ Class ReportsModel extends MasterModel
 
     public function generateReportOrdersWithCompanyAndRol(int $IdCompany = null, int $rol_id = null, $date_init, $date_end)
     {
-        $sql = "SELECT DISTINCT `order`.order_id, `order`.order_date, `order`.order_name, `order`.order_subtotal, `order`.order_total, `order`.order_iva, extra_attributes_order.order_attrs_name, extra_attributes_order.order_attrs_desc, company.*, users.*,
-            (SELECT SUM(order_subtotal) FROM `order`) AS total_subtotal,
-            (SELECT SUM(order_total) FROM `order`) AS total_total,
-            (SELECT SUM(order_iva) FROM `order`) AS total_iva
-            FROM `order`
-            INNER JOIN users
-                ON users.u_id = `order`.u_id
-            INNER JOIN company
-                ON company.c_id = users.c_id
-            INNER JOIN order_articles
-                ON order_articles.order_id = `order`.order_id
-            LEFT JOIN extra_attributes_order
-                ON extra_attributes_order.order_id = `order`.order_id
-            WHERE `order`.order_date BETWEEN :date_init AND :date_end";
+        $sql    = "SELECT DISTINCT
+                        `orders`.order_id,
+                        `orders`.order_date,
+                        `orders`.order_name,
+                        `orders`.order_subtotal,
+                        `orders`.order_total,
+                        `orders`.order_iva,
+                        extra_attributes_order.order_attrs_name,
+                        extra_attributes_order.order_attrs_desc,
+                        company.*, users.*,
+                        (SELECT SUM(order_subtotal) FROM `orders`) AS total_subtotal,
+                        (SELECT SUM(order_total) FROM `orders`) AS total_total,
+                        (SELECT SUM(order_iva) FROM `orders`) AS total_iva
+                    FROM
+                        `orders`
+                        INNER JOIN users ON users.u_id = `orders`.u_id
+                        INNER JOIN company ON company.c_id = users.c_id
+                        INNER JOIN order_articles ON order_articles.order_id = `orders`.order_id
+                        LEFT JOIN extra_attributes_order ON extra_attributes_order.order_id = `orders`.order_id
+                    WHERE
+                        `orders`.order_date BETWEEN :date_init AND :date_end";
     
         $params = [':date_init' => $date_init, ':date_end' => $date_end];
     
         if ($rol_id === 3 || $rol_id === 4) {
             $sql .= " AND company.c_id = :c_id";
-            $params[':c_id'] = $IdCompany;
+
+            $params[':c_id']    = $IdCompany;
         }
     
-        $report = $this->select($sql, $params);
-    
-        return $report;
+        return $this->select($sql, $params);
     }
     public function generateReportStockWithCompanyAndRol($date_init,$date_end){
         $sql="SELECT articles.ar_id,articles.ar_name,
